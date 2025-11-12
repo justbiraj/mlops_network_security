d462088d900e3b777205730b3fe2a5d74e83b311
Biraj Mishra <silub33@gmail.com>
2025-11-12 16:23:03 +0545

    updated config file o pipeline

---
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -0,0 +1,27 @@
+import os
+import sys
+import numpy as np
+import pandas as pd
+
+"""
+Data Ingestion related constant vaariables
+"""
+
+DATA_INGESTION_COLLECTION_NAME: str = "network_data"
+DATA_INGESTION_DATABASE_NAME: str = "network_security_db"
+DATA_INGESTION_DIR_NAME: str = "data_ingestion"
+DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
+DATA_INGESTION_INGESTED_DIR: str = "ingested"
+DATA_INGESTION_TRAIN_TEST_SPLIT_RATION: float = 0.2
+
+
+"""
+Defining common constrain for training pipeline
+"""
+TARGET_COLUMN:str = "Result"
+PIPELINE_NAME: str = "network_security_pipeline"
+ARTIFACT_DIR: str = "Artifact"
+FILE_NAME: str = "phishingData.csv"
+
+TRAIN_FILE_NAME: str = "train.csv"
+TEST_FILE_NAME: str = "test.csv"
